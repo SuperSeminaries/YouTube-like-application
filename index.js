bef8a1ba5ceed017cff3469ca60317348366e00d
shubham varma <shubhamvarma793@gmail.com>
2024-03-20 12:11:57 +0530

    mongoDB connection

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello");
+import { app } from "./app.js";
+import { connectDB } from "./src/db/index.js";
+import dotenv from "dotenv";
+dotenv.config()
+
+const port = process.env.PORT || 5000
+
+
+connectDB()
+.then(() => {
+    app.listen(port, () => {
+        console.log(`server listen on port: ${port}`);
+    })
+})
+.catch((error) => {
+    console.log("mongoDB connection faild !!!", error);
+})
---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../../constants.js"
+
+
+
+const connectDB = async () => {
+    try {
+        const connection = await  mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        console.log(`mongoDB connected || DB  HOST: ${connection.connection.host}`);
+    } catch (error) {
+        console.log("masseg", error);
+    }
+}
+
+export { connectDB }
